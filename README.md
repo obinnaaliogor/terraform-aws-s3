992184269ea158dc95a488e1ee5ba9a75bf09a00
aliogorobinna <aliogor7509@yahoo.com>
2023-04-12 08:29:58 +0200

    s3 bucket with kms_key,server_side_encryption,Versioning and random name

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 ### Repository for terraform projects! 
 
+
+A ReadMe file with a brief description about the repository
+Set branch protection rule for merging on PR
+Set your aws credentials as repository secrets
+s set.
 #### 1st Project:
+## Create a repository for terraform projects
 #### Creating an s3 bucket with kms encryption for terraform backend.
 #### This will be used to store terraform statefile for security and for DR.
+Create an s3 to be used as a backend for state storage
+Bucket needs to have server side encryption with kms
+The bucket name needs to conform to bootcamp30-$(random-number)-(yourname)
+
+
+AC: A repository for terraform modules with credential
--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,33 +1,71 @@
 provider "aws" {
   region = "us-east-2"
 }
-# Create a KMS key for S3 server-side encryption
-resource "aws_kms_key" "s3_key" {
-  description = "KMS key for S3 server-side encryption"
-}
 
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "obinnatfstate-bucket"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-   server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3_key.key_id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+resource "aws_s3_bucket" "backend" {
+  count = var.create_vpc ? 1 : 0 #Count a meta argument that changes/alters the state/behavior of a resource
+#We are using with "?" a tenary operator to check for the condition of our defined variable
+# ? --> conditions, checks if the create_vpc = true it should create one bucket else dont create which is "0"
+  bucket =  random_pet.pet_name.id    #bootcamp30-$(random-number)-(yourname)
+  #We used the resource random_pet to generate the name and a range of number
 
   tags = {
-    Name   =  "TfBucket"
-    Environment = "Dev"
+    Name = "Dev"
+    Environment = "HR"
   }
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.mybucket.id
+resource "aws_s3_bucket_ownership_controls" "owner_controls" {
+  bucket = aws_s3_bucket.backend[0].id #b/c of the condition we added we had to introduce index to anywhere we see bucket referenced
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
+resource "aws_s3_bucket_acl" "myacl" {
+  depends_on = [aws_s3_bucket_ownership_controls.owner_controls]
+
+  bucket = aws_s3_bucket.backend[0].id
+  acl    = "private"
+}
+
+resource "aws_kms_key" "backendkey" {
+  description             = "KMS key for backend bucket"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = aws_s3_bucket.backend[0].id
+  rule {
+    apply_server_side_encryption_by_default {
+    sse_algorithm = "aws:kms"
+    kms_master_key_id = aws_kms_key.backendkey.arn
+  }
+}
+}
+#Since versioning is deprecated by passing it directly, we are creating it as a resource..
+resource "aws_s3_bucket_versioning" "backend_version" {
+  bucket = aws_s3_bucket.backend[0].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+#Changing the bucket name to random names
+
+resource "random_pet" "pet_name" { #randompet has prefix can any name length can be any number
+  prefix = "bootcamp30"
+  length = 2
+
+}
+
+/*resource "random_integer" "num" {
+  max = 100
+  min = 2
+  
+}*/
+#Lets use conditions to determine weather a bucket should be created or not
+
+variable "create_vpc" {
+  type = bool
+  default = true
+}
